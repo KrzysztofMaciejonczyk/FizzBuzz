e3f27a00f0085bf8ae966a075e8edb7d6cc36c7a
KrzysztofMaciejonczyk <119799230+KrzysztofMaciejonczyk@users.noreply.github.com>
2024-02-28 23:09:57 +0100

    Add files via upload

---
+++ FizzBuzz/Program.cs
@@ -0,0 +1,11 @@
+﻿namespace FizzBuzz
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            FizzBuzz fz1 = new FizzBuzz(30);
+            fz1.Display();
+        }
+    }
+}
